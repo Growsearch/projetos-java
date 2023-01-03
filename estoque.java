0b40580549073f0cedabb3672e66b974e94a8906
Grow Search <marcelocx1904@gmail.com>
2023-01-03 18:15:49 -0300

    Update estoque.java

---
+++ estoque.java
@@ -0,0 +1,80 @@
+
+package entities;
+
+public class product {
+
+	
+	public String name;
+	public double price;
+	public double quantity;
+	
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+	
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+	
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+	
+	public String toString() {
+		return name
+		+ ", $"
+		+ String.format("%.2f", price)
+		+ ", "
+		+ quantity
+		+ "units, Total: $ "
+		+ String.format("%.2f", totalValueInStock());
+	}
+}
+
+// =========== PROGRAMA ========== //
+
+package estoqueloja;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.product;
+
+public class program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		product product = new product();
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		product.name = sc.nextLine();
+		System.out.print("price: ");
+		product.price = sc.nextDouble();
+		System.out.print("Quantity in stock: ");
+		product.quantity = sc.nextInt();
+		
+		System.out.println();
+		System.out.println("Product data: " + product);
+		
+		System.out.println();
+		System.out.println("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Update data: " + product);
+		
+		System.out.println();
+		System.out.println("Enter the number of product to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Update data: " + product);
+		
+		sc.close();
+	}
+
+}
